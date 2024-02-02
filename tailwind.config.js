fb62a5e5bdb1eea868d09e7941350945eddc78bd
francis <franzdiaz460@gmail.com>
2024-02-02 06:59:53 +0400

    setting up custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      black: "#1d1d1d",
+      white: "#ffffff",
+      primary: {
+        main: "#E2C7BF",
+        button: "#CD8D7A",
+        link: "#DCB1A4",
+      },
+      accent: {
+        yellow: {
+          300: "#DBCC95",
+        },
+        green: {
+          100: "#B8E3CF",
+          200: "#EAECCC",
+          300: "#C3E2C2",
+        },
+      },
+    },
   },
   plugins: [],
 };
