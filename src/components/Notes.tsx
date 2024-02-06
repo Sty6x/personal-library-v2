bbfe35fca27c88f502c1d8f7d32d546d40d9af75
francis <franzdiaz460@gmail.com>
2024-02-06 09:23:25 +0400

    ref: appended note components

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,6 +1,6 @@
 const Note = ({ contents }: { contents: string }) => {
   return (
-    <div className="w-full note text-pretty mb-3  text-2xl grid place-items-center h-[max-content] border-solid border-b-black py-6 border-b-2">
+    <div className="cursor-pointer w-full note text-pretty mb-3 text-2xl grid h-[max-content] border-solid border-b-black py-6 border-b-2">
       {contents}
     </div>
   );
--- src/views/book/page/Page.tsx
+++ src/views/book/page/Page.tsx
@@ -1,15 +1,12 @@
 import { Link, useOutletContext, useParams } from "react-router-dom";
-import {
-  t_book,
-  t_note,
-  t_currentBook,
-  t_page,
-} from "../../../types/t_library";
+import { t_note, t_page } from "../../../types/t_library";
 import { useEffect, useState } from "react";
 import { data } from "../../../placeholderData";
 import getRelatedItems from "../../../utils/getRelatedItems";
 import BookHeader from "../../../components/BookHeader";
 import { AnimatePresence } from "framer-motion";
+import Note from "../../../components/Notes";
+import { motion } from "framer-motion";
 
 type t_currentPage = {
   currentPage: t_page;
@@ -51,13 +48,24 @@ const Page = () => {
     console.log(pageData);
   }, [pageData]);
 
+  const renderNotes = pageData?.notes.map((note) => {
+    return (
+      <Note
+        key={note.id}
+        contents={
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        }
+      />
+    );
+  });
+
   // flex pushes the horizontal and vertical center of the component to the center
   return (
     <div
       id="page"
       className="max-w-[1440px] h-[90%] flex flex-col justify-start mx-16 overflow-hidden "
     >
-      <header className="sticky top-0 border-b-black border-b-4 shadow-header border-solid bg-white bg-gridWhite py-6">
+      <header className="sticky top-0 border-b-black border-b-2 shadow-header border-solid bg-white bg-gridWhite py-6">
         <Link to={"/library"} className="underline text-lg">
           Go back to library
         </Link>
@@ -66,27 +74,30 @@ const Page = () => {
           author={pageData?.book.author as string}
         />
         <p className="text-xl">Page {pageData?.currentPage.pageNum}</p>
-        <span className="flex gap-4 items-center mt-3">
+        <motion.span
+          whileHover={{ x: 10 }}
+          className="flex gap-4 items-center mt-3"
+        >
           <Link
-            className="add-icon w-[max-content] before:mr-[.3em] items-center before:h-[20px] relative flex content-center bg-primary-link  rounded-sm shadow-btn-hover hover:shadow-btn-hover-active transition-shadow hover:transition-shadow duration-200 px-4 py-1 text-lg"
+            className="add-icon w-[max-content] before:mr-[.3em] items-center before:h-[20px] relative flex content-center font-semibold py-1 text-xl"
             to={"/#"}
           >
             Add Note
           </Link>
-          <Link
+          {/* <Link
             to={"/library"}
             className="add-icon w-[max-content] before:mr-[.3em] items-center before:h-[20px] relative flex content-center bg-accent-green-200  rounded-sm shadow-btn-hover hover:shadow-btn-hover-active transition-shadow hover:transition-shadow duration-200 px-4 py-1 text-lg"
           >
             Add Page
-          </Link>
-        </span>
+          </Link> */}
+        </motion.span>
       </header>
       <section className="">
         <div
           id="notes-container"
           className="max-h-[50em] w-[90em] justify-start overflow-y-auto"
         >
-          <AnimatePresence custom={"popLayout"}></AnimatePresence>
+          <AnimatePresence custom={"popLayout"}>{renderNotes}</AnimatePresence>
         </div>
       </section>
     </div>
