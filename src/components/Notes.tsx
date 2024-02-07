185bd5b30b64c2fccc11d93b62086f20955a2465
francis <franzdiaz460@gmail.com>
2024-02-07 14:21:47 +0400

    ref: integrating quilljs for note editing

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,4 +1,8 @@
 import { motion } from "framer-motion";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+
 interface t_dragEvents {
   onDragStart: (e: React.DragEvent<HTMLDivElement>) => void;
   onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
@@ -12,6 +16,8 @@ const Note = ({
   id: string;
   dragEvents: t_dragEvents;
 }) => {
+  const [editorState, setEditorState] = useState<string>(contents);
+
   function onDragEnter(e: React.DragEvent<HTMLDivElement>): void {
     const dropTarget = e.currentTarget;
     if (!dropTarget.classList.contains("droppable")) {
@@ -33,20 +39,30 @@ const Note = ({
   }
 
   return (
-    <motion.div
-      whileTap={{ scale: 0.95 }}
-      id={id}
-      draggable={true}
-      onDragStart={onDragStart}
-      onDrop={onDrop}
-      onDragOver={onDragOver}
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDragEnd={onDragLeave}
-      className="relative z-10 outline-[3px] cursor-pointer w-full note text-pretty mb-3 text-xl grid h-[max-content] border-solid border-b-note-separator py-4 border-b-[1px]"
-    >
-      {contents}
-    </motion.div>
+    <div>
+      {true ? (
+        <ReactQuill
+          theme="snow"
+          value={editorState}
+          onChange={setEditorState}
+        />
+      ) : (
+        <motion.div
+          whileTap={{ scale: 0.95 }}
+          id={id}
+          draggable={true}
+          onDragStart={onDragStart}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          onDragEnter={onDragEnter}
+          onDragLeave={onDragLeave}
+          onDragEnd={onDragLeave}
+          className="relative z-10 outline-[3px] cursor-pointer w-full note text-pretty mb-3 text-xl grid h-[max-content] border-solid border-b-note-separator py-4 border-b-[1px]"
+        >
+          {contents}
+        </motion.div>
+      )}
+    </div>
   );
 };
 export default Note;
