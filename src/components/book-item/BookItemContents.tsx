464faa8b5b8453ba3b13a495f37a2bb03efcd6be
francis <franzdiaz460@gmail.com>
2024-02-21 22:32:26 +0400

    style: adding appropriate width for the title

--- src/components/book-item/BookItemContents.tsx
+++ src/components/book-item/BookItemContents.tsx
@@ -1,16 +1,35 @@
-import { t_book, t_currentBook } from "../../types/t_library";
+import { formatDistance } from "date-fns/formatDistance";
+import { t_book, t_currentBook, t_note, t_page } from "../../types/t_library";
+import getRelatedItems from "../../utils/getRelatedItems";
+import LibraryStorage from "../../utils/Library";
 
-const BookItemContents = ({ book }: { book: t_book | t_currentBook }) => {
+const BookItemContents = ({
+  book,
+  recentNote,
+  recentPage,
+}: {
+  book: t_book | t_currentBook;
+  recentNote?: t_note;
+  recentPage?: t_page;
+}) => {
   return (
     <>
-      <span className="max-w-[400px] leading-7 text-3xl min-[1930px]:text-[2.5rem] max-[1280px]:text-[1.4rem] max-[1280px]:leading-[1.4rem] font-bold">
-        {book.title}
+      <span className="text-sm font-semi-bold">
+        Last updated {formatDistance(new Date(book.lastUpdated), new Date())}{" "}
+        ago.
       </span>
-      <span className="text-md min-[1930px]:text-[1.4rem] max-[1440px]:text-[1rem] font-semibold">
-        by {book.author}
+      <span className="my-1">
+        <div className="text-3xl w-full leading-[1.8rem] font-bold">
+          <h1 className=" text-pretty max-w-[300px]">{book.title}</h1>
+        </div>
+        <span className="text-md font-semibold">by {book.author}</span>
       </span>
-      <span className="text-md min-[1930px]:text-[1rem]  max-[1440px]:text-[.8rem] font-semi-bold">
-        Pages {book.pageIDs.length} • Notes {3}
+      <span className="text-sm font-semi-bold">
+        {recentNote && recentPage
+          ? `Page #${recentPage?.pageNum} • Note #${recentNote?.noteNum}`
+          : `Pages written ${book.pageIDs.length} • Notes created ${
+              getRelatedItems<t_note>(book.pageIDs, LibraryStorage.notes).length
+            }`}
       </span>
     </>
   );
