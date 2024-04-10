ea2236c18f2f1545bae68a53749f243b8a4628c3
francis-lp-win <franzdiaz460@gmail.com>
2024-04-10 17:31:58 +0400

    ref: contained Note Preview

--- src/components/book-item/BookItem.tsx
+++ src/components/book-item/BookItem.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const BookItemContentsLayout = ({ children }: { children: ReactNode }) => {
@@ -21,6 +21,57 @@ const BookItemContentsLayout = ({ children }: { children: ReactNode }) => {
     >
       {children}
     </motion.div>
+  );
+};
+
+const NotePreview = ({
+  deps: { link, children, noteContents },
+}: {
+  deps: { [key: string]: any };
+}) => {
+  return (
+    <motion.a
+      href={link}
+      className="
+       hover:cursor-pointer
+       hover:shadow-book-item-active
+       hover:transition-shadow
+       transition-shadow
+       hover:duration-200
+       duration-200
+       shadow-book-item
+       gap-4 py-6 px-8
+       min-w-[200px] min-h-[250px] max-w-[500px] w-full
+       max-[1200px]:min-h-[230px]
+       max-[1200px]:px-6
+       max-[1200px]:py-4
+       bg-white rounded-md
+       box-content"
+      key="contents"
+      initial={{ opacity: 0, scale: 0, display: "none" }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+        transition: {
+          delay: 0.2,
+        },
+        display: "grid",
+      }}
+      exit={{
+        opacity: 0,
+        scale: 0,
+        transition: { duration: 0.2 },
+      }}
+    >
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col">{children}</div>
+        <div className=" h-full bg-black rounded p-3">
+          <p className="max-[1200px]:text-[.8rem] max-[1200px]:leading-4 max-[1280px]:line-clamp-5  max-[1280px]:text-[1rem] text-white text-pretty note-contents-preview">
+            {noteContents}
+          </p>
+        </div>
+      </div>
+    </motion.a>
   );
 };
 
@@ -63,7 +114,10 @@ const BookItem = ({
         </Link>
       ) : (
         <motion.div
-          onMouseEnter={() => setIsHovered(true)}
+          onMouseEnter={() => {
+            console.log("Entered");
+            setIsHovered(true);
+          }}
           onMouseLeave={() => setIsHovered(false)}
           key={motionKey}
           style={{ backgroundColor: color }}
@@ -78,49 +132,7 @@ const BookItem = ({
                 {children}
               </BookItemContentsLayout>
             ) : (
-              <motion.a
-                href={link}
-                className="
-            hover:cursor-pointer
-            hover:shadow-book-item-active
-            hover:transition-shadow
-            transition-shadow
-            hover:duration-200
-            duration-200
-            shadow-book-item
-            gap-4 py-6 px-8
-            min-w-[200px] min-h-[250px] max-w-[500px] w-full
-            max-[1200px]:min-h-[230px]
-            max-[1200px]:px-6
-            max-[1200px]:py-4
-            bg-white rounded-md
-            box-content
-            "
-                key="contents"
-                initial={{ opacity: 0, scale: 0, display: "none" }}
-                animate={{
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.2,
-                  },
-                  display: "grid",
-                }}
-                exit={{
-                  opacity: 0,
-                  scale: 0,
-                  transition: { duration: 0.2 },
-                }}
-              >
-                <div className="flex flex-col gap-2">
-                  <div className="flex flex-col">{children}</div>
-                  <div className=" h-full bg-black rounded p-3">
-                    <p className="max-[1200px]:text-[.8rem] max-[1200px]:leading-4 max-[1280px]:line-clamp-5  max-[1280px]:text-[1rem] text-white text-pretty note-contents-preview">
-                      {noteContents}
-                    </p>
-                  </div>
-                </div>
-              </motion.a>
+              <NotePreview deps={{ children, noteContents, link }} />
             )}
           </AnimatePresence>
         </motion.div>
