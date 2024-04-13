186bddaca7b7fbf060d37e5db7da1c4689996dce
francis-lp-win <franzdiaz460@gmail.com>
2024-04-13 10:34:21 +0400

    style: readjusted book header

--- src/components/BookHeader.tsx
+++ src/components/BookHeader.tsx
@@ -1,7 +1,7 @@
 const BookHeader = ({ title, author }: { title: string; author: string }) => {
   return (
     <div
-      className={`${title.length < 50 ? "min-[800px]:max-w-[26em]" : "min-[800px]:max-w-[38em]"} max-[800px]:max-w-[20em] flex flex-col max-[400px]:w-full `}
+      className={`${title.length < 50 ? "min-[800px]:max-w-[38em]" : "min-[800px]:max-w-[40em]"} max-[650px]:max-w-[25em] max-[800px]:max-w-[30em] flex flex-col max-[500px]:w-full `}
     >
       <span className=" inline-block">
         <h1 className="text-6xl max-sm:text-4xl max-md:text-5xl max-[320px]:text-2xl max-[320px]:leading-7 font-bold line-clamp-4">
